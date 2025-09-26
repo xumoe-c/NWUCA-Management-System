693734e216d2a1b23d1c19e2399960e74a98e947
JiomLan <65555262+youcharamahua@users.noreply.github.com>
2025-09-26 12:43:01 +0800

    Refactor: 初始化前端结构，采用Vue3

--- client/api/api.js
+++
@@ -1,9 +0,0 @@
-// api.js
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1', // 后端服务地址
-    timeout: 5000,
-});
-
-export default instance;
--- client/main.js
+++ client/main.js
@@ -1,8 +1,22 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
 import { createSSRApp } from 'vue'
-import App from './App.vue'
 export function createApp() {
-    const app = createSSRApp(App)
-    return {
-        app
-    }
+  const app = createSSRApp(App)
+  return {
+    app
+  }
 }
+// #endif
